ab897d009a14d6dc697b0db22459cec9c124263e
Andreas Schmitz <schmitz@terrestris.de>
2018-08-08 16:50:46 +0200

    add support for targetBands parameters

--- src/main/java/de/terrestris/snappy/SnappyScriptGenerator.java
+++ src/main/java/de/terrestris/snappy/SnappyScriptGenerator.java
@@ -8,8 +8,7 @@ import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Pattern;
 
 import static org.apache.logging.log4j.LogManager.getLogger;
@@ -23,14 +22,74 @@ public class SnappyScriptGenerator {
 
     private static Pattern NUMBER = Pattern.compile("[0-9]+\\.?[0-9]*");
 
+    private static class TargetBand {
+        private String name;
+        private String type;
+        private String expression;
+        private String description;
+        private String unit;
+        private String noDataValue;
+    }
+
+    private static void parseBands(XMLStreamReader in, Map<String, Object> map) throws XMLStreamException {
+        Map<String, Object> bands = new TreeMap<>();
+        int i = 0;
+        while (!(in.isEndElement() && in.getName().equals(new QName("targetBands")))) {
+            if (!(in.isStartElement() && in.getLocalName().equals("targetBand"))) {
+                in.next();
+                continue;
+            }
+            String bandName = "targetBand" + ++i;
+            TargetBand band = new TargetBand();
+            bands.put(bandName, band);
+            while (!(in.isEndElement() && in.getName().equals(new QName("targetBand")))) {
+                if (in.isStartElement() && !in.getName().equals(new QName("targetBand"))) {
+                    String name = in.getLocalName();
+                    String text = in.getElementText();
+
+                    if (name.equals("name")) {
+                        band.name = text;
+                    }
+                    if (name.equals("type")) {
+                        band.type = text;
+                    }
+                    if (name.equals("expression")) {
+                        band.expression = text;
+                    }
+                    if (name.equals("description")) {
+                        band.description = text;
+                    }
+                    if (name.equals("unit")) {
+                        band.unit = text;
+                    }
+                    if (name.equals("noDataValue")) {
+                        band.noDataValue = text;
+                    }
+                }
+                in.next();
+            }
+        }
+        in.next();
+        map.put("targetBands", bands);
+    }
+
     private static Map<String, Object> parseParameters(XMLStreamReader in) throws XMLStreamException {
         Map<String, Object> map = new HashMap<>();
         in.next();
         while (!(in.isEndElement() && in.getName().equals(new QName("parameters")))) {
             if (in.isStartElement()) {
                 try {
-                    String text = in.getElementText();
                     String name = in.getLocalName();
+                    if (name.equals("targetBands")) {
+                        parseBands(in, map);
+                        continue;
+                    }
+                    String text = in.getElementText();
+                    if (text == null || text.isEmpty()) {
+                        LOG.info("Skipping element '{}' because its value is empty.", name);
+                        in.next();
+                        continue;
+                    }
                     if (BOOLEAN.matcher(text).matches()) {
                         map.put(name, Boolean.parseBoolean(text));
                     } else if (NUMBER.matcher(text).matches()) {
@@ -40,6 +99,7 @@ public class SnappyScriptGenerator {
                     }
                 } catch (Throwable e) {
                     LOG.info("Skipping element '{}' due to parsing error '{}'.", in.getLocalName(), e.getMessage());
+                    LOG.debug("Stack trace: ", e);
                 }
             }
             in.next();
@@ -76,7 +136,7 @@ public class SnappyScriptGenerator {
         out.println("# generated operations follow");
     }
 
-    private static void generateNodeCode(String operator, Map<String,Object> map, PrintWriter out) {
+    private static void generateNodeCode(String operator, Map<String, Object> map, PrintWriter out) {
         if (operator.equals("Read")) {
             out.println("####################");
             out.println("# read data from file");
@@ -98,8 +158,10 @@ public class SnappyScriptGenerator {
             out.println("parameters = HashMap()");
             for (Map.Entry<String, Object> e : map.entrySet()) {
                 Object value = e.getValue();
-                if (value instanceof Boolean) {
-                    out.println("parameters.put('" + e.getKey() + "', " + (((Boolean)value) ? "True" : "False") + ")");
+                if (value instanceof Map) {
+                    writeTargetBands((Map) value, out);
+                } else if (value instanceof Boolean) {
+                    out.println("parameters.put('" + e.getKey() + "', " + (((Boolean) value) ? "True" : "False") + ")");
                 } else if (value instanceof Double) {
                     out.println("parameters.put('" + e.getKey() + "', " + value + ")");
                 } else if (e.getKey().equals("mapProjection")) {
@@ -118,6 +180,45 @@ public class SnappyScriptGenerator {
             out.println("print '" + operator + " is done.'");
             out.println();
         }
+    }
+
+    private static void writeTargetBands(Map<String, TargetBand> map, PrintWriter out) {
+        out.println();
+        out.println("# create target bands object");
+        out.println("BandDescriptor = jpy.get_type('org.esa.snap.core.gpf.common.BandMathsOp$BandDescriptor')");
+
+        List<String> names = new ArrayList<>();
+        map.forEach((name, band) -> {
+            out.println();
+            out.println(name + " = BandDescriptor()");
+            if (band.name != null && !band.name.isEmpty()) {
+                out.println(name + ".name = '" + band.name + "'");
+            }
+            if (band.type != null && !band.name.isEmpty()) {
+                out.println(name + ".type = '" + band.type + "'");
+            }
+            if (band.expression != null && !band.expression.isEmpty()) {
+                out.println(name + ".expression = '" + band.expression + "'");
+            }
+            if (band.description != null && !band.description.isEmpty()) {
+                out.println(name + ".description = '" + band.description + "'");
+            }
+            if (band.unit != null && !band.unit.isEmpty()) {
+                out.println(name + ".unit = '" + band.unit + "'");
+            }
+            if (band.noDataValue != null && !band.noDataValue.isEmpty()) {
+                out.println(name + ".noDataValue = " + band.noDataValue);
+            }
+            names.add(name);
+        });
+        out.println();
+        out.println("targetBands = jpy.array('org.esa.snap.core.gpf.common.BandMathsOp$BandDescriptor', " + map.size() + ")");
+        for (int i = 0; i < map.size(); ++i) {
+            out.println("targetBands[" + i + "] = " + names.get(i));
+        }
+
+        out.println();
+        out.println("parameters.put('targetBands', targetBands)");
     }
 
     public static void generate(InputStream xml, OutputStream py) throws XMLStreamException, UnsupportedEncodingException {
